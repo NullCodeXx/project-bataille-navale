6c0a5fad05e0aa52d2424d58655ef171f33fa15c
SOolOo CrakOo <SOolOoCrakOo@xxxx-xxxx-xxxx.home>
2017-07-27 15:46:54 +0200

    color table ok

--- view/scripts.js
+++ view/scripts.js
@@ -9,30 +9,15 @@ LA BATAILLE NAVALE.
 		let table = document.querySelector(".table");
 		for(let x = 0; x < 10; x++) {
 		let tr = document.createElement("tr");
+		//css.
+		tr.classList.add("table-tr");
 		table.appendChild(tr);
 		for(let n = 0; n < 10; n++) {
 			let td = document.createElement("td");
+			//css.
+			td.classList.add("table-td");
 			tr.appendChild(td);
 			td.innerHTML = x * 10 + (n + 1);
 		}
 	}
 }
-
-function css() {
-        let table = document.querySelector("td");
-        table.style.backgroundColor = "red";
-}
-// //MODIF DISPLAY.
-// function displayChange() {
-// 	let tr = document.querySelectorALL(tr);
-// 	let td = document.querySelectorALL(td);
-// 	tr.classList.add(".table-tr");
-// 	td.classList.add(".table-td");
-// }
-
-
-
-
-
-
-
